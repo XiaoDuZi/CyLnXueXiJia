185557681e84bc6647736498441496b7e4a47728
DuXinwu <13262083380@163.com>
2017-09-05 11:10:05 +0800

    Initial commit

---
+++ app/src/main/java/com/cy/cylnxuexijia/CyLnXueXiJiaMainActivity.java
@@ -0,0 +1,13 @@
+package com.cy.cylnxuexijia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CyLnXueXiJiaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cy_ln_xue_xi_jia_main);
+    }
+}
